8e14a5c600320c518e31af4964e1347196af2826
Ethan Binkley <rossebinkley@gmail.com>
2024-05-30 00:41:20 -0400

    changed urls for website pages

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -13,10 +13,10 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/Portfolio/" element={<Home />} />
+        <Route path="/Portfolio/about" element={<About />} />
+        <Route path="/Portfolio/projects" element={<Projects />} />
+        <Route path="/Portfolio/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
